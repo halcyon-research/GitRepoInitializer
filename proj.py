99b1a91492b0079fceddd77e67019754ea1e1cbb
michaelpeterswa <michaelpeterswa@icloud.com>
2019-06-20 15:15:48 -0700

    modularized and refactored

--- proj.py
+++ proj.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 # # # # # # # # # # # #
-#                _    #
+#               _    #
 #     ___ _____(_)    #
 #    / _ `/ __/ /     #
 #    \_, /_/ /_/      #
@@ -16,65 +16,109 @@ import os
 import argparse
 import json
 import emoji
+from datetime import datetime
 
-data = {}  
-data['project'] = []
-parser = argparse.ArgumentParser()
-parser.add_argument("init")
-args = parser.parse_args()
-
-if args.init:
-    name = input('Project Name: ')
-    strippedName = name.replace(" ", "")
-    author = input('Author: ')
-    version = input('Version: ')
-    description = input('Description: ')
-    os.system('clear')
-    os.system('mkdir ' + strippedName)
-    os.chdir(strippedName)
-    os.system('git init')
-
-    f = open("README.md", "w")
-    f.write("# " + strippedName + "\n")
-    f.write(description + "\n")
-    f.write("## Author" + "\n")
-    f.write(author + "\n") 
-    f.close()
-
-    githubName = input("Enter Github Username: ")
-
-    command = "curl -u '" + githubName + "' https://api.github.com/user/repos -d '{\"name\":\"" + strippedName + "\"}'"
-
-    #print(command)
-    #curl -u 'USER' https://api.github.com/user/repos -d '{"name":"REPO"}'
-    #git remote add origin git@github.com:USER/REPO.git
-    #https://github.com/michaelpeterswa/testProject4.git
-    #git push origin master
+def prettyPrinter(name, auth, ver, desc, longDesc, date, link):
+    name = ":fire: Repository Name: " + name
+    auth = ":books: Author: " + auth
+    ver = ":bar_chart: Version: " + ver
+    desc = ":page_facing_up: Description: " + desc
+    longDesc = ":clipboard: Long Description: " + longDesc
+    date = ":date: Date: " + date
+    message = ":zap: Repository Successfully Initialized :zap:"
     
-    gitRemote = "https://github.com/" + githubName + "/" + strippedName + ".git"
-    gitLink = "https://github.com/" + githubName + "/" + strippedName 
+    print(addEmoji(message))
+    print('')
+    print(addEmoji(name))
+    print(addEmoji(auth))
+    print(addEmoji(ver))
+    print(addEmoji(desc))
+    print(addEmoji(longDesc))
+    print(addEmoji(date))
+    print('')
+    print(link)
 
-    data['project'].append({'author': author,
+def addEmoji(str):
+    return emoji.emojize(str, use_aliases=True)
+
+def writeToJSON(name, auth, ver, desc, longDesc, date, link):
+    data = {}  
+    data['project'] = []
+
+    data['project'].append({'author': auth,
                             'projectName': name,
-                            'version': version,
-                            'description': description,
-                            'githubLink': gitLink
+                            'version': ver,
+                            'description': desc,
+                            'longDescription': longDesc,
+                            'githubLink': link,
+                            'date': date
                             })
 
     with open('info.json', 'w') as outfile:  
         json.dump(data, outfile, indent=4)
 
+def writeReadme(name, auth, ver, desc, longDesc, date, link):
+    f = open("README.md", "w")
+    f.write("# " + name + "\n")
+    f.write(desc + "\n")
+    f.write("> " + longDesc + "\n")
+    f.write("## Installation" + "\n")
+    f.write("```" + "\n") 
+    f.write("git clone " + link + "\n")        
+    f.write("```" + "\n") 
+    f.write("Or download the file manually." + "\n")
+    f.write("## Release History" + "\n")
+    f.write("* " + ver + "\n")
+    f.write("   * " + "Opened Repository " + date + "\n")
+    f.write("## Meta" + "\n")
+    f.write(auth + " - *enter additional contact information here*" + "\n") 
+    f.write("\nDistributed under the ____ license. See ``LICENSE`` for more information.")
+    f.close()
+
+#---------------------------------
+
+parser = argparse.ArgumentParser()
+parser.add_argument("init")
+args = parser.parse_args()
+
+if args.init:
+
+    #INPUTS
+    name = input('Project Name: ')
+    strippedName = name.replace(" ", "")
+    author = input('Author: ')
+    version = input('Version: ')
+    description = input('Short Description: ')
+    longDescription = input('Long Description: ')
+    githubName = input("Enter Github Username: ")
+    date = datetime.today().strftime('%m.%d.%Y')
+    dateFormatted = "("+ date + ")"
+
+    #create, enter, and init project directory
+    os.system('clear')
+    os.system('mkdir ' + strippedName)
+    os.chdir(strippedName)
+    os.system('git init')
+
+    #format repository opening request
+    command = "curl -u '" + githubName + "' https://api.github.com/user/repos -d '{\"name\":\"" + strippedName + "\"}'"
+
+    #format two links to the repository
+    gitRemote = "https://github.com/" + githubName + "/" + strippedName + ".git"
+    gitLink = "https://github.com/" + githubName + "/" + strippedName 
+
+    #create first files
+    writeReadme(name, author, version, description, longDescription, dateFormatted, gitRemote)
+    writeToJSON(name, author, version, description, longDescription, date, gitLink)
+
+    #add, commit, create repository, connect remote, and push
     os.system('git add .')
-    os.system('git commit -m "first commit made with RepoCreator"')
+    os.system('git commit -m "first commit made with GitRepoInitializer"')
     os.system(command)
     os.system('git remote add origin ' + gitRemote)
     os.system('git push -u origin master')
 
+    #show that the process has completed successfully
     os.system('clear')
-    print(emoji.emojize(':zap: Repository Creator Finished Successfully :zap:', use_aliases=True))
-    print("Repository name: " + strippedName)
-    print("Author: " + author)
-    print("Version: " + version)
-    print("Description: " + description)
-    print("")
-    print(gitLink)
+    prettyPrinter(strippedName, author, version, description, longDescription, date, gitLink)
+
